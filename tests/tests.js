1875932c3166f6a065f8c0c415c3f8d0852b2ea7
Payton Lower <paytown@me.com>
2019-02-25 16:22:11 -0800

    first function test passing

--- tests/tests.js
+++ tests/tests.js
@@ -1,3 +1,34 @@
 const test = QUnit.test;
 
 import './html-equal.js';
+
+function makeHtmlTemplate() {
+    const template = document.createElement('template');
+    template.innerHTML = `
+        <li>
+            <h2>UniWhal</h2>
+            <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
+        </li>
+        `;
+
+    return template.content;
+}
+
+
+
+test('test template HTML to match coded HTML', assert => {
+    
+    
+    const result = makeHtmlTemplate();
+
+    const expected = `
+        <li>
+            <h2>UniWhal</h2>
+            <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
+        </li>
+        `;
+
+    // assert
+    assert.htmlEqual(result, expected);
+
+});
