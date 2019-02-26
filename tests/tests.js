8c94f4edd69924a23ae95761bec494f396af199f
Payton Lower <paytown@me.com>
2019-02-25 16:27:50 -0800

    function written, passes tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,17 +1,12 @@
 const test = QUnit.test;
 
 import './html-equal.js';
+import makeHtmlTemplate from '../src/make-html-template.js';
 
-function makeHtmlTemplate() {
-    const template = document.createElement('template');
-    template.innerHTML = `
-        <li>
-            <h2>UniWhal</h2>
-            <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
-        </li>
-        `;
 
-    return template.content;
+const image = {
+    title: 'UniWhal',
+    url: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'
 }
 
 
@@ -19,7 +14,7 @@ function makeHtmlTemplate() {
 test('test template HTML to match coded HTML', assert => {
     
     
-    const result = makeHtmlTemplate();
+    const result = makeHtmlTemplate(image);
 
     const expected = `
         <li>
