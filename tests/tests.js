fad54dfa822013e78012687c5c80853a4b8e5575
Payton Lower <paytown@me.com>
2019-02-25 17:14:44 -0800

    fixed for travis

--- tests/tests.js
+++ tests/tests.js
@@ -7,13 +7,9 @@ import makeHtmlTemplate from '../src/make-html-template.js';
 const image = {
     title: 'UniWhal',
     url: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'
-}
-
-
+};
 
 test('test template HTML to match coded HTML', assert => {
-    
-    
     const result = makeHtmlTemplate(image);
 
     const expected = `
